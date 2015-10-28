f72947c5dd013edfee2d062fdb805eeb03d8e87b
Artem <dutchnewsru@gmail.com>
2015-10-28 16:08:37 +0100

    Update update.php

--- update.php
+++ update.php
@@ -1,11 +1,39 @@
+<?php
+include_once ('user.php');
+$object = new User();
+
+if($object->isLoggedIn() && isset($_POST['submit'])) {
+	$firstname = $_POST['firstname'];
+	$lastname = $_POST['lastname'];
+	$email = $_POST['email'];
+	$username = $_POST['username'];
+	$password = $_POST['password'];
+	$object->Update();
+	
+
+} else {
+
+	echo "Something is wrong";
+}
+	{
+	$object->getUser();
+					echo " Put into your updated details
+
 <html>
 <body>
-<form method="post" action="index.php">
-Username: <input type="text" name="user"/><br><br>
-Password: <input type="text" name="pass"/><br><br>
-Email: <input type="text" name="email"/><br><br>
-<button class="btn btn-large btn-danger" id="alertMe" input type="submit" name="update" value="register">Update</button>
+<form method=\"post\" action=\"index.php\">
+Firstname: <input type=\"text\" name=\"firstname\"/><br><br>
+Lastname: <input type=\"text\" name=\"lastname\"/><br><br>
+Email: <input type=\"text\" name=\"email\"/><br><br>
+Username: <input type=\"text\" name=\"username\"/><br><br>
+Password: <input type=\"text\" name=\"password\"/><br><br>
+<button class=\"btn btn-large btn-danger\" id=\"alertMe\" input type=\"submit\" name=\"submit\" value=\"update\">Update</button>
 
 </form>
 </body>
-</html>
+</html>";
+
+
+}
+
+?>
